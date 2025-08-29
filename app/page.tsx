5af6edf026d9e519df7ed32b54918129efe78ebb
Michael Paonam <michael.paonam@sap.com>
2025-08-29 18:28:56 +0530

    chore: changed title

--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,11 @@ function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        {/* Blog. */} Home
       </h1>
       <h2 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
-        <a
+        {/* A statically generated blog example using*/} {""}
+        {/* <a
           href="https://nextjs.org/"
           className="underline hover:text-success duration-200 transition-colors"
         >
@@ -30,7 +30,7 @@ function Intro() {
         >
           {CMS_NAME}
         </a>
-        .
+        . */}
       </h2>
     </section>
   );
